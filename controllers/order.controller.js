73deb5485c579c0c8d02131b57c62ad054d50dc1
= <=>
2023-05-26 23:24:46 +0300

    fix

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -14,7 +14,7 @@ router.get('/:user_id', validateDbId, (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    orderMethods.create(req.body.user_id, req.body.items, req.body.shippingAddres, req.body.total)
+    orderMethods.create(req.body.user_id, req.body.items, req.body.paymentStatus, req.body.shippingAddres, req.body.total)
         .then(data => res.status(201).json(data))
         .catch(err => next(err))
 });
--- services/order.service.js
+++ services/order.service.js
@@ -2,49 +2,28 @@ const User = require('../models/user.model');
 
 exports.generateMethods = Model => {
     return {
-        create: async (user_id, items, shippingAddress, total) => {
-            // let user = await User.findOne({ _id: { $eq: user_id } })
+        create: async (user_id, items, paymentStatus, shippingAddress, total) => {
             return new Promise((resolve, reject) => {
-                Model.find({ user_id: { $eq: user_id } }).then(res => {
-                    if (res.length != 0) {
-                        Model.updateOne({ user_id: user_id }, {
+                let newRecord = {
+                    user_id: user_id,
+                    paymentStatus: paymentStatus,
+                    status: '',
+                    total: total,
+                    items: items,
+                    shippingAddress: shippingAddress
+                }
+                Model.create(newRecord).then(async res => {
+                    if (newRecord.shippingAddress.save) {
+                        await User.updateOne({ _id: user_id }, {
                             $set: {
                                 "shippingAddress": shippingAddress
                             }
                         }).then(res => {
-                            if (shippingAddress.save) {
-                                User.updateOne({ _id: user_id }, {
-                                    $set: {
-                                        "shippingAddress": shippingAddress
-                                    }
-                                }).then(res => {
-                                    resolve(true)
-                                })
-                            }
-                        })
-                    } else {
-                        let newRecord = {
-                            user_id: user_id,
-                            paymentStatus: '',
-                            status: '',
-                            total: total,
-                            items: items,
-                            shippingAddress: shippingAddress
-                        }
-                        Model.create(newRecord).then(async res => {
-                            if (newRecord.shippingAddress.save) {
-                                await User.updateOne({ _id: user_id }, {
-                                    $set: {
-                                        "shippingAddress": shippingAddress
-                                    }
-                                }).then(res => {
-                                    resolve(true)
-                                })
-                            }
                             resolve(true)
-                        });
+                        })
                     }
-                })
+                    resolve(true)
+                });
             });
         },
 
